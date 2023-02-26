65ac569b77041d2b8107967ca930827b6484cc0e
hirdle <ilyapower368@gmail.com>
2023-02-26 19:00:14 +0300

    fixing

---
+++ database.py
@@ -0,0 +1,30 @@
+import pysondb
+db = pysondb.getDb("dialogs.json")
+
+
+def getDialog(id):
+    dialog = db.getByQuery({"userid": id})
+    if dialog == []:
+        createDialog(id)
+        dialog = db.getByQuery({"userid": id})
+    return dialog[0]
+
+def createDialog(id):
+    db.add({"userid": id, "messages": []})
+    return getDialog(id)
+
+def addDialog(id, q, a):
+
+    dialog = getDialog(id)
+
+    if dialog == []:
+        dialog = createDialog(id)
+
+    dialog['messages'].append(q)
+    dialog['messages'].append(a)
+
+    db.updateByQuery({"userid": id}, {"messages": dialog['messages']})
+    return dialog
+
+def clearDialog(id):
+    db.updateByQuery({"userid": id}, {"messages": []})
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import logging
 import config
 
-import openai
+from database import getDialog, addDialog, clearDialog
 
-openai.api_key = config.API_TOKEN_OPENAI
-model_engine = "text-davinci-003"
-
+import requests
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
@@ -14,6 +12,7 @@ logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=config.API_TOKEN_BOT)
 dp = Dispatcher(bot)
+
 
 
 def create_keyboard(keys={}, backBtn=False):
@@ -30,17 +29,31 @@ def create_keyboard(keys={}, backBtn=False):
     return keyboard
 
 
-def get_chatgpt_data(prompt):
-    completion = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
+def get_chatgpt_data(prompt, history):
 
-    return completion.choices[0].text
+    error = ""
+
+    try:
+        url = "https://api.openai.com/v1/completions"
+
+        context = "\n".join(history)
+
+        data = {
+            "model": "text-davinci-003",
+            "prompt": "context: "+context+"\n\nprompt: "+prompt,
+            "max_tokens": 1000,
+            "temperature": 0,
+        }
+
+        headers = {'Accept': 'application/json', 'Authorization': 'Bearer '+config.API_TOKEN_OPENAI}
+        r = requests.post(url, headers=headers, json=data)
+
+        error = r.text
+
+        return r.json()['choices'][0]['text'].strip()
+
+    except Exception as e:
+        return f"Возникли некоторые трудности.\nПопробуйте очистить чат: /clear"
 
 
 check_subscrition_keyboard = create_keyboard({"Проверить подписку":"check_subs",'Оформить "Премиум"':"buy_subs"})
@@ -99,21 +112,36 @@ async def start_function_callback(callback_query: CallbackQuery):
     await bot.send_message(callback_query.from_user.id, config.start_text, reply_markup=start_keyboard)
 
 
+@dp.message_handler(commands=['clear'])
+async def clear_chat_function(message: types.Message):
+
+    clearDialog(message.from_user.id)
+
+    await message.answer("Диалог успешно очищен.")
+
 
 @dp.message_handler()
 async def handle_any_text_message(message: types.Message):
     
     if await check_subscrition(message):
         return
-    
-    await message.answer(config.await_text)
-    await message.answer(get_chatgpt_data(message.text))
+
+    sticker = types.InputFile.from_url("https://sticker-collection.com/stickers/animated/chatex2/whatsapp/99e1571a-181a-49ce-a6a6-97ec32002809file_3942631.webp")
+    message_sticker = await bot.send_sticker(chat_id=message.chat.id, sticker=sticker)
+
+
+    dialog = getDialog(message.from_user.id)['messages']
+
+    chatgpt_response = get_chatgpt_data(
+        prompt=message.text,
+        history=dialog
+    )
+
+    addDialog(message.from_user.id, message.text, chatgpt_response)
+
+    await message.answer(chatgpt_response)
     await message.answer(config.start_text, reply_markup=start_keyboard)
-
-
-
-
-
+    await bot.delete_message(chat_id=message.chat.id, message_id=message_sticker.message_id)
 
 
 if __name__ == '__main__':
